e9b3a7cedcce621da4080cdff5d8f141ddc80f0a
Sebastian Menze <sebastian.menze@gmail.com>
2025-11-20 11:55:43 +0100

    Update __init__.py

--- src/pase/__init__.py
+++ src/pase/__init__.py
@@ -1,1 +1,16 @@
-from pase import pase
+"""
+Python Audio Spectrogram Explorer (PASE)
+A GUI tool for marine acoustic analysis and annotation
+
+Created by Sebastian Menze
+Email: sebastian.menze@gmail.com
+"""
+
+__version__ = "1.0.0"
+__author__ = "Sebastian Menze"
+__email__ = "sebastian.menze@gmail.com"
+__license__ = "MIT"
+
+from .pase import start
+
+__all__ = ["start"]
